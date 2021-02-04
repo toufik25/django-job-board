f1d672728738911474724b4b6a512910461c5b87
unknown <toufik.cherrani@gmail.com>
2021-02-04 21:21:49 +0100

    add job views urls

--- job/admin.py
+++ job/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Job
+from .models import Category
 
 admin.site.register(Job)
+admin.site.register(Category)
--- job/models.py
+++ job/models.py
@@ -15,12 +15,24 @@ JOB_TYPE=(
 )
 class Job(models.Model): #table
     title= models.CharField(max_length=100) #column
+    #location
     job_type= models.CharField(max_length=15,choices=JOB_TYPE)
     description=models.TextField(max_length=1000)
     published_at=models.DateTimeField(auto_now=True)
     vacancy=models.IntegerField(default=1)
     salary=models.IntegerField(default=0)
     experience=models.IntegerField(default=1)
-
+    category=models.ForeignKey('Category',on_delete=models.CASCADE) #'Category' it is added in singel cotations because the class Category is  define later
+    
+    
+    # to add the title of the jobs as default instead of the default job_....
     def __str__(self):
         return self.title
+
+
+class Category (models.Model):
+    name=models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+        
